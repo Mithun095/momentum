516afdb613652aded41cc4102c4a142ea8c13433
mithun095 <mithunmallya095@gmail.com>
2026-03-04 17:59:04 +0530

    refactor: Migrate database from SQLite to PostgreSQL, update CI configuration, and remove unused debug scripts.

--- debug-env.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-console.log('Current Directory:', process.cwd());
-console.log('NODE_ENV:', process.env.NODE_ENV);
-console.log('GEMINI_API_KEY Present:', !!process.env.GEMINI_API_KEY);
-if (process.env.GEMINI_API_KEY) {
-    console.log('GEMINI_API_KEY Length:', process.env.GEMINI_API_KEY.length);
-    console.log('GEMINI_API_KEY Start:', process.env.GEMINI_API_KEY.substring(0, 5));
-}
-console.log('GROK_API_KEY Present:', !!process.env.GROK_API_KEY);
